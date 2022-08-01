2f76370d64000879b845c14164650df133e9a5d8
sindyvaness <42687025+sindyvaness@users.noreply.github.com>
2022-08-01 09:48:48 -0500

    Pruebas Unitarias controller  Get

--- APIRestProductManagement.Test/UnitsTests/ProductsControllerTest.cs
+++ APIRestProductManagement.Test/UnitsTests/ProductsControllerTest.cs
@@ -9,7 +9,7 @@ namespace APIRestProductManagement.Test.UnitsTests
     {       
 
         [TestMethod]
-        public void GetTest()
+        public async Task GetTest()
         {
             // Preparacion
             var bdName = Guid.NewGuid().ToString();
@@ -29,11 +29,31 @@ namespace APIRestProductManagement.Test.UnitsTests
 
             });
 
-            ProductsController productsController = new ProductsController(context, mapper);
+            context.Products.Add(new Product()
+            {
+                Id = new Guid(),
+                Description = "PRODUCTO 6",
+                Status = 1,
+                ManufacturingDate = Convert.ToDateTime("2022/01/01"),
+                ValidityDate = Convert.ToDateTime("2022/12/31"),
+                SupplierCode = 1,
+                SupplierDescription = "PROVEEDOR ABC",
+                SupplierPhone = "12345"
+
+            });
+
+            await context.SaveChangesAsync();
+
+            var conext2= BuildDbContext(bdName);
 
             // Ejecucion
+            var controller = new ProductsController(context, mapper);
+
+            var answer = await controller.Get();
 
             //Verificacion
+            var productCount = answer.Value;
+            Assert.AreEqual(2, productCount.Count);
         }
     }
 }
